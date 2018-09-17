ed239a5d62af5250b75e2331a7be03e8859e33a9
Kiyoshi Yodogawa <kiyoshiyodogawa@keyrus.com.br>
2018-09-17 16:40:18 -0300

    [TELA-INICIAL] Header com Menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,6 @@ export default class Header extends Component {
         return (
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Welcome to React</h1>
                 <Menu />
             </header>
         );
