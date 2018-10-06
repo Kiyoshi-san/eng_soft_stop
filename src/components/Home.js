8a1a3c5ad93336c5e820fdc9683adee4fe1d97b5
João Pedro <joao_p221@oultook.com>
2018-10-06 11:55:27 -0300

    Criação menu e estrutura inicial do projeto - preparação CI/CD

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,6 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                <Button variant="primary">Primary</Button>
             </div>
         )
     }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,97 +1,30 @@
 import React, { Component } from 'react';
+import Nav from 'react-bootstrap/lib/Nav';
 import Navbar from 'react-bootstrap/lib/Navbar';
+import NavDropdown from 'react-bootstrap/lib/NavDropdown';
 
-import Dropdown from "./Dropdown";
 import "../css/menu.css";
 
 export default class Menu extends Component {
-    constructor(props) {
-        super (props);
-        this.state = {
-            listOpen: false, 
-            listaMenu: [
-                {
-                    id: 0,
-                    title: "Home",
-                    selected: false,
-                    key: "location",
-                    itens: null
-                }, {
-                    id: 1,
-                    title: "Loja",
-                    selected: false,
-                    key: "location",
-                    itens: null
-                }, {
-                    id: 2,
-                    title: "Perfil",
-                    selected: false,
-                    key: "location",
-                    itens: [
-                        {
-                            id: 0,
-                            title: "Conta",
-                            selected: false,
-                            key: "location"
-                        },{
-                            id: 1,
-                            title: "Personalizar",
-                            selected: false,
-                            key: "location"
-                        },{
-                            id: 2,
-                            title: "Itens",
-                            selected: false,
-                            key: "location"
-                        },{
-                            id: 3,
-                            title: "Sair",
-                            selected: false,
-                            key: "location"
-                        }
-                    ]
-                }
-            ]
-        }
+    constructor() {
+        super ();
     }
 
-    handleClickOutside() {
-        this.setState({
-            listOpen: false
-        })
-    }
-
-    toggleList(id) {
-        if (id === 2)
-            this.setState(prevState => ({
-                listOpen: !prevState.listOpen
-            }))
-    }
-    
     render () {
-        const { listaMenu, listOpen } = this.state;
         return (
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Stop App</Navbar.Brand>
-                <Navbar.Toggle />
-                <Navbar.Collapse className="justify-content-end">
-                    <Navbar.Text>
-                        {listaMenu.map((item) => (
-                            {item.itens === null &&
-                            }
-                        ))}
-                    </Navbar.Text>
-                </Navbar.Collapse>
-                
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="menu">
+            <Navbar.Brand href="/">Stop App</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                </Nav>
+                <Nav className="menu-itens">
+                <Nav.Link href="#">Home</Nav.Link>
+                <Nav.Link eventKey={2} href="#">Loja</Nav.Link>
+                <Nav.Link eventKey={3} href="#">Perfil</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
             </Navbar>
-            {<ul className="menu-list">
-                {listaMenu.map((item) => (
-                    <li className="menu-list-item" key={item.id} onClick={() => this.toggleList(item.id) }>
-                        {item.title} 
-                        {item.id === 2 && <Dropdown list={item.itens} listOpen={listOpen} />}
-                    </li>
-                ))}
-            </ul>}
         );
     }
 }
