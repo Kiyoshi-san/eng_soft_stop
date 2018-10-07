3f6b634ec4752c751b793dbf89d66ec561628b8d
kiyo_yodo <kiyoshibudokan@hotmail.com>
2018-10-07 05:32:38 -0300

    [BACKOFFICE] Tela Backoffice

--- src/components/shared/Menu.js
+++ src/components/shared/Menu.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Nav from 'react-bootstrap/lib/Nav';
 import Navbar from 'react-bootstrap/lib/Navbar';
 
+import logo from '../../images/stop_logo.png';
+
 import "../../css/menu.css";
 
 export default class Menu extends Component {
@@ -12,7 +14,10 @@ export default class Menu extends Component {
     render () {
         return (
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="menu">
-            <Navbar.Brand href="/">Stop App</Navbar.Brand>
+            {/* <Navbar.Brand href="/">Stop App</Navbar.Brand> */}
+            <Navbar.Brand href="/">
+                <img src={logo} className="logo_stop" alt="logo" />
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
