597d5acb6eeba09048d0edddbac7483eb6b18277
João Pedro <joao_p221@oultook.com>
2018-10-07 23:34:16 -0300

    Criação de formulário de login

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/lib/Button';
+import Card from 'react-bootstrap/lib/Card';
+import Form from 'react-bootstrap/lib/Form';
 
-
+import "../../css/login.css";
 
 export default class Login extends Component {
     constructor(props) {
@@ -11,9 +14,30 @@ export default class Login extends Component {
 
     render() {
         return (
-            <div>
-                login
+            <div className="login-body">
+                <Card className="login-card">
+                <Card.Body>
+                    <Card.Title>Login do Usuário</Card.Title>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="email" />
+                        <Form.Text className="text-muted">
+                        Use aqui seu email e senha cadastrados.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Senha</Form.Label>
+                        <Form.Control type="password" placeholder="senha" />
+                    </Form.Group>
+                    <Form.Group id="formBasicChecbox">
+                        <Form.Check type="checkbox" label="Mantenha-me logado" />
+                    </Form.Group>
+                    <Button variant="primary">Login</Button>
+                </Card.Body>
+                </Card>
             </div>
-        )
+
+        );
     }
 }
