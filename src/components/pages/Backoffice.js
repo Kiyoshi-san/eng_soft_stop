a4694e854c909287d19d5ec02342e932cd5bc94b
kiyo_yodo <kiyoshibudokan@hotmail.com>
2018-10-08 01:24:26 -0300

    [

--- src/components/pages/Backoffice.js
+++ src/components/pages/Backoffice.js
@@ -1,27 +1,60 @@
 import React, { Component } from 'react';
-
+import '../../css/backoffice.css';
 import axios from "axios";
 
 export default class Backoffice extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            categoria: ""
-            
+            categorias: [],
+            categ_val: "",
+            palavra: "",
+            addPalavra: null,
+            componentePalavra: []
         };
+    }
+    
+    componentDidMount() {
+        axios
+            .get('https://jsonplaceholder.typicode.com/users')
+            /* .then(res => {
+                this.setState({ 
+                    categorias: res.data 
+                })
+            }) */
+
+            this.setState({ 
+                categorias: [{
+                    id: 1,
+                    categoria: "Carro"
+                }, {
+                    id: 2,
+                    categoria: "Cor"
+                }, {
+                    id: 3,
+                    categoria: "CEP"
+                }, {
+                    id: 4,
+                    categoria: "Marca"
+                }]
+            })
     }
 
     handleChange = e => {
         this.setState({ 
-            categoria: e.target.value 
+            categ_val: e.target.value,
+            palavra: e.target.value
         });
+        console.log(e.target.value);
     }
     
+    /* Enviando dados para salvar */
     handleSubmit = e => {
         e.preventDefault();
 
         const cadastroCategoria = {
-            categoria: this.state.categoria
+            categ_val: this.state.categ_val,
+            palavras: this.state.palavra
         }
 
         axios
@@ -32,15 +65,35 @@ export default class Backoffice extends Component {
             }) 
             
     }
+
+
+    addComponentePalavra = (res) => {
+        //create a unike key for each new componentePalavra item
+        var timestamp = (new Date()).getTime();
+        // update the state object
+        this.state.componentePalavra['componentePalavra-' + timestamp] = res;
+        // set the state
+        this.setState({ 
+            componentePalavra: this.state.componentePalavra 
+        });
+    }
+
     
     render() {
         return (
             <div>
                 <form onSubmit={ this.handleSubmit}>
-                    <label>
+                    <button onClick={ this.addPalavra } type="button">Add mais palavras</button>
+                    {/* <label className="inputBkofc">
                         Categoria: <input type="text" name="categoria" onChange={this.handleChange }/>
+                    </label> */}
+                    <label className="inputBkofc">
+                        Categoria: <select name="categoria" onChange={ this.handleChange }>
+                            <option selected value="">Selecione</option>
+                            { this.state.categorias.map(res => <option value={ res.id }>{ res.categoria }</option>) }
+                        </select>
                     </label>
-                    <label>
+                    <label className="inputBkofc">
                         Palavra: <input type="text" name="palavra" onChange={this.handleChange }/>
                     </label>
                     <button type="submit">Add</button>
--- src/components/shared/Menu.js
+++ src/components/shared/Menu.js
@@ -14,20 +14,20 @@ export default class Menu extends Component {
     render () {
         return (
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="menu">
-            {/* <Navbar.Brand href="/">Stop App</Navbar.Brand> */}
-            <Navbar.Brand href="/">
-                <img src={logo} className="logo_stop" alt="logo" />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                </Nav>
-                <Nav className="menu-itens">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link eventKey={2} href="#">Loja</Nav.Link>
-                <Nav.Link eventKey={3} href="/login">Login</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
+                {/* <Navbar.Brand href="/">Stop App</Navbar.Brand> */}
+                <Navbar.Brand href="/">
+                    <img src={logo} className="logo_stop" alt="logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                    </Nav>
+                    <Nav className="menu-itens">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link eventKey={2} href="#">Loja</Nav.Link>
+                        <Nav.Link eventKey={3} href="/login">Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         );
     }
