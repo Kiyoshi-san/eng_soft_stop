3de0a8d46b21166a7ca4f7c37a9826f4ea2d24ce
kiyo_yodo <kiyoshibudokan@hotmail.com>
2018-10-18 16:43:21 -0300

    [BACKOFFICE] Refatoracao

--- src/components/pages/BackPalavra.js
+++ src/components/pages/BackPalavra.js
@@ -54,7 +54,7 @@ export default class BackPalavra extends Component {
         this.palavrasList();
     }
 
-    enviarCadastro = cadastroResposta => {
+    enviarCadastroResposta = cadastroResposta => {
         let valor = cadastroResposta.value;
         if (valor.trim()) {
             axios
@@ -77,10 +77,27 @@ export default class BackPalavra extends Component {
         }
     }
 
-    /* Enviando dados para salvar */
-    handleSubmit = e => {
+    enviarCadastroCategoria = cadastroCategoria => {
+        let valor = cadastroCategoria;
+        if (valor.trim()) {
+            axios
+                // .post(`${'https://cors-anywhere.herokuapp.com/'}https://es3-stop-prod.herokuapp.com/answer`, { "category_id":this.state.category_id, "description":valor })
+                .post('https://es3-stop-prod.herokuapp.com/category', { "name":valor })
+                .then(res => {
+                    alert("Cadastrado com sucesso");
+                    document.getElementsByName("categoriaName")[0].value = "";
+                    this.categoryList();
+                })
+                .catch(res => {
+                    alert("Algo deu errado")
+                })
+        }
+    }
+
+    /* Enviando Resposta para salvar */
+    handleSubmitResposta = e => {
         e.preventDefault();
-        alert("")
+        
         // const cadastroResposta = new FormData(e.target);
         // const cadastroResposta = new FormData(this.form);
         
@@ -100,8 +117,26 @@ export default class BackPalavra extends Component {
         // window.location.reload();
         selectCategoria = 0;
         // cadastroResposta.forEach((a) => a.value = "");
-        this.enviarCadastro(cadastroResposta);
+        this.enviarCadastroResposta(cadastroResposta);
         cadastroResposta = "";
+    }
+
+    /* Enviando Categoria para salvar */
+    handleSubmitCategoria = e => {
+        e.preventDefault();
+        
+        // const cadastroResposta = new FormData(e.target);
+        // const cadastroResposta = new FormData(this.form);
+        
+        let cadastroCategoria = document.getElementsByName("categoriaName")[0].value;
+                
+        if (!cadastroCategoria.trim()) {
+            alert("Por favor preencha uma categoria válida");
+            return;
+        }
+        
+        // cadastroResposta.forEach((a) => a.value = "");
+        this.enviarCadastroCategoria(cadastroCategoria);
     }
 
     excluir = e => {
@@ -191,65 +226,95 @@ export default class BackPalavra extends Component {
             <Row className="backContainer">
             {/* <div className="row bck--container"> */}
                 <h1 class="bkfcTitulo">Cadastro</h1>
-                <div className="col-xs-12 col-md-8">
+                <div className="col-xs-12 col-sm-12">
                     <form className="container" autoComplete="off"/*  onSubmit={ this.handleSubmit} */>
-                        <div className="container botoes">
-                            {/* <button className="btn btn-primary botao" onClick={this.addComponentePalavra} type="button">Add mais Respostas</button> */}
+                        {/* Categorias */}
+                        <div className="backContainer row">
+                            <div className="col-xs-12 col-sm-6">
+                                <div className="container botoes">
+                                    {/* <button className="btn btn-primary botao" onClick={this.addComponentePalavra} type="button">Add mais Respostas</button> */}
 
-                            {this.state.componentePalavra.length ?
-                                <button className="btn btn-danger botao" type="button" onClick={this.deleteComponentePalavra}>Remover palavra</button> : ""}
-                                {console.log(this.state.componentePalavra)}
+                                    {this.state.componentePalavra.length ?
+                                        <button className="btn btn-danger botao" type="button" onClick={this.deleteComponentePalavra}>Remover palavra</button> : ""}
+                                        {console.log(this.state.componentePalavra)}
 
-                            {/* <button className="btn btn-success botao">Enviar</button> */}
-                        </div>
+                                    {/* <button className="btn btn-success botao">Enviar</button> */}
+                                </div>
 
-                        {/* <label className="inputBkofc">
-                            Categoria: <input type="text" name="categoria" onChange={this.handleChange }/>
-                        </label> */}
-                        <label className="inputBkofc">
-                            Categoria: <select name="categoria" className="form-control selectClass" onChange={ this.handleChange }>
-                                <option selected value={0}>Selecione</option>
-                                { this.state.categorias.map(res => <option value={ res.category_id }>{ res.name }</option>) }
-                            </select>
-                        </label>
-                        {/* <label className="inputBkofc">
-                            Palavra: <input type="text" name="description" className="form-control" onChange={this.handleChange }/>
-                        </label> */}
+                                {/* <label className="inputBkofc">
+                                    Categoria: <input type="text" name="categoria" onChange={this.handleChange }/>
+                                </label> */}
 
-                        { Object.keys(this.state.componentePalavra).map(function(key) {
-                            return (
-                                <label className="inputBkofc">
-                                    Palavra: <input type="text" name="description" className="form-control" /* onChange={this.handleChange } *//>
-                                    {/* <button type="button" data-idx={ Object.keys(this.state.componentePalavra).length - 1 } onClick={ this.deleteComponentePalavra }>X</button> */}
-                                </label>
-                            )
-                        }.bind(this)) }
+                                <table className="table table__categ">
+                                    <thead className="thead-dark">
+                                        <tr>
+                                            <th scope="col">Categorias</th>
+                                            <th scope="col"></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="col"><input type="text" name="categoriaName" placeHolder="Adicionar uma Categoria" className="form-control" onChange={this.handleChange} /></th>
+                                            <th scope="col"><button className="btn btn-success botao" onClick={this.handleSubmitCategoria}/*  type="submit" */>Enviar</button></th>
+                                        </tr>
+                                        { this.state.categorias.map(res => {
+                                            return (
+                                                <tr>
+                                                    <td>{res.name}</td>
+                                                    <td><button className="btn-danger" value={res.category_id} onClick={this.excluir}>-</button></td>
+                                                </tr>
+                                            )
+                                        }) }
+                                    </tbody>
+                                </table>
+                            </div>
+                            {/* <label className="inputBkofc">
+                                Palavra: <input type="text" name="description" className="form-control" onChange={this.handleChange }/>
+                            </label> */}
 
-                    </form>
-                </div>
-                <div className="col-xs-12 col-md-4">
-                    <table class="table">
-                        <thead class="thead-dark">
-                            <tr>
-                                <th scope="col">Respostas</th>
-                                <th scope="col"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="col"><input type="text" name="description" place className="form-control" onChange={this.handleChange} /></th>
-                                <th scope="col"><button className="btn btn-success botao" onClick={this.handleSubmit}/*  type="submit" */>Enviar</button></th>
-                            </tr>
-                            {this.state.listaPalavras.map(res => {
+                            { Object.keys(this.state.componentePalavra).map(function(key) {
                                 return (
-                                    <tr>
-                                        <td>{res.description}</td>
-                                        <td><button className="btn-danger" value={res.category_id} onClick={this.excluir}>-</button></td>
-                                    </tr>
+                                    <label className="inputBkofc">
+                                        Palavra: <input type="text" name="description" className="form-control" /* onChange={this.handleChange } *//>
+                                        {/* <button type="button" data-idx={ Object.keys(this.state.componentePalavra).length - 1 } onClick={ this.deleteComponentePalavra }>X</button> */}
+                                    </label>
                                 )
-                            }) }
-                        </tbody>
-                    </table>
+                            }.bind(this)) }
+
+                            {/* Respostas */}
+                            <div className="col-xs-12 col-sm-6">
+                                <table class="table">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                            <th scope="col">Respostas</th>
+                                            <th scope="col">
+                                                <label className="inputBkofc">
+                                                    <select name="categoria" className="form-control selectClass" onChange={ this.handleChange }>
+                                                        <option selected value={0}>Selecione uma Categoria</option>
+                                                        { this.state.categorias.map(res => <option value={ res.category_id }>{ res.name }</option>) }
+                                                    </select>
+                                                </label>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="col"><input type="text" name="description" placeHolder="Adicionar uma Resposta" className="form-control" onChange={this.handleChange} /></th>
+                                            <th scope="col"><button className="btn btn-success botao" onClick={this.handleSubmitResposta}/*  type="submit" */>Enviar</button></th>
+                                        </tr>
+                                        {this.state.listaPalavras.map(res => {
+                                            return (
+                                                <tr>
+                                                    <td>{res.description}</td>
+                                                    <td><button className="btn-danger" value={res.category_id} onClick={this.excluir}>-</button></td>
+                                                </tr>
+                                            )
+                                        }) }
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             {/* </div> */}
             </Row>
