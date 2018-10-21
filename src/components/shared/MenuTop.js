a3739c36cf8ea75823d70e6afe77c2cbc549a103
Daniel do Vale <dovale.daniel07@gmail.com>
2018-10-21 19:59:52 -0300

    Commit para correção de build

--- src/components/shared/MenuTop.js
+++ src/components/shared/MenuTop.js
@@ -3,10 +3,10 @@ import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLi
 import { BrowserRouter as Router } from 'react-router-dom';
 import logo from '../../images/stop_logo_v2.png';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
-import { faStoreAlt } from '@fortawesome/free-solid-svg-icons'
-import { faHome } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faUser } from '@fortawesome/free-solid-svg-icons'
+// import { faStoreAlt } from '@fortawesome/free-solid-svg-icons'
+// import { faHome } from '@fortawesome/free-solid-svg-icons'
 
 export default class MenuTop extends React.Component {
     constructor(props) {
@@ -34,14 +34,14 @@ export default class MenuTop extends React.Component {
                     <Collapse isOpen = { this.state.collapse } navbar>
                         <NavbarNav right>
                           <NavItem active>
-                              <NavLink to="/"><FontAwesomeIcon icon={faHome} /> Home</NavLink>
+                              {/* <NavLink to="/"><FontAwesomeIcon icon={faHome} /> Home</NavLink> */}
                           </NavItem>
                           <NavItem>
-                              <NavLink to="/loja"><FontAwesomeIcon icon={faStoreAlt} /> Loja</NavLink>
+                              {/* <NavLink to="/loja"><FontAwesomeIcon icon={faStoreAlt} /> Loja</NavLink> */}
                           </NavItem>
                           <NavItem>
                             <Dropdown>
-                                <DropdownToggle nav caret><FontAwesomeIcon icon={faUser} /> Perfil</DropdownToggle>
+                                {/* <DropdownToggle nav caret><FontAwesomeIcon icon={faUser} /> Perfil</DropdownToggle> */}
                                 <DropdownMenu>
                                     <DropdownItem href="/conta">Conta</DropdownItem>
                                     <DropdownItem href="/itens">Itens</DropdownItem>
