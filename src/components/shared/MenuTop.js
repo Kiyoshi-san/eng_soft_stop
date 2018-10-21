7dab8c7fb8622ea4f3711bffc7778a799500090b
kiyo_yodo <kiyoshibudokan@hotmail.com>
2018-10-21 01:28:24 -0300

    [MENU-TOP]

---
+++ src/components/shared/MenuTop.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+export default class MenuTop extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapse: false,
+            isWideEnough: false,
+        };
+    this.onClick = this.onClick.bind(this);
+    }
+
+    onClick(){
+        this.setState({
+            collapse: !this.state.collapse,
+        });
+    }
+    render() {
+        return (
+            <Router>
+                <Navbar color="indigo" dark expand="md" scrolling>
+                    <NavbarBrand href="/">
+                        <strong>Navbar</strong>
+                    </NavbarBrand>
+                    { !this.state.isWideEnough && <NavbarToggler onClick = { this.onClick } />}
+                    <Collapse isOpen = { this.state.collapse } navbar>
+                        <NavbarNav left>
+                          <NavItem active>
+                              <NavLink to="#">Home</NavLink>
+                          </NavItem>
+                          <NavItem>
+                              <NavLink to="#">Features</NavLink>
+                          </NavItem>
+                          <NavItem>
+                              <NavLink to="#">Pricing</NavLink>
+                          </NavItem>
+                          <NavItem>
+                            <Dropdown>
+                                <DropdownToggle nav caret>Dropdown</DropdownToggle>
+                                <DropdownMenu>
+                                    <DropdownItem href="#">Action</DropdownItem>
+                                    <DropdownItem href="#">Another Action</DropdownItem>
+                                    <DropdownItem href="#">Something else here</DropdownItem>
+                                    <DropdownItem href="#">Something else here</DropdownItem>
+                                </DropdownMenu>
+                            </Dropdown>
+                          </NavItem>
+                        </NavbarNav>
+                        <NavbarNav right>
+                          <NavItem>
+                            <form className="form-inline md-form mt-0">
+                              <input className="form-control mr-sm-2 mb-0 text-white" type="text" placeholder="Search" aria-label="Search"/>
+                            </form>
+                          </NavItem>
+                        </NavbarNav>
+                    </Collapse>
+                </Navbar>
+            </Router>
+        );
+    }
+}
